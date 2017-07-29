3b4373ab1110e41c34755c452f17f60f0a668069
Saurabh Shah <saurabhshah.0410@gmail.com>
2017-07-29 18:58:38 +0530

    Added the mirror method

--- BinaryTree.java
+++ BinaryTree.java
@@ -131,7 +131,22 @@ public class BinaryTree {
 		}
 		System.out.println();
 	}
-	
+	public void mirror()
+	{
+		mirror(root);
+	}
+	private void mirror(Node node)
+	{
+		if(node != null) 
+		{
+			mirror(node.left);
+			mirror(node.right);
+			Node temp = node.left;
+			node.left = node.right;
+			node.right = temp;
+		}
+		
+	}
 }
 
 
